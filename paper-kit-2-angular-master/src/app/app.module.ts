313d72a3605466c99c2525b775f64103c5a57dc7
khalil1fs <aboulouanek@gmail.com>
2022-05-17 00:28:40 +0100

    add sommethings

--- paper-kit-2-angular-master/src/app/app.module.ts
+++ paper-kit-2-angular-master/src/app/app.module.ts
@@ -10,31 +10,13 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 
 import { ComponentsModule } from './components/components.module';
-// import { ExamplesModule } from './examples/examples.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterComponent } from './register/register.component';
-// import { PanierComponent } from './module/client/view/panier/panier.component';
-// import { CheckoutComponent } from './checkout/checkout.component';
-// import { ProductsComponent } from './module/client/view/products/products.component';
-// import { ProductCreateComponent } from './module/client/view/products/product-create/product-create.component';
-// import { ProductListComponent } from './module/client/view/products/product-list/product-list.component';
-import { AuthComponent } from './auth/auth.component';
 import { DemoComponent } from './demo/demo.component';
-// import { PagesComponent } from './pages/pages.component';
-// import { UtilsComponent } from './utils/utils.component';
-// import { ModuleComponent } from './module/module.component';
-
-
-// import { ClientComponent } from './module/client/client.component';
-// import { AdminComponent } from './module/admin/admin.component';
-// import { VisitorComponent } from './module/visitor/visitor.component';
-// import { ViewComponent } from './module/admin/view/view.component';
 import { FormationComponent } from './module/client/view/formation/formation.component';
 import { ProduitBioComponent } from './module/client/view/produit-bio/produit-bio.component';
 import { CongresComponent } from './module/client/view/congres/congres.component';
-// import { PanierModel } from './module/client/view/panier/panier.model';
-import { PanierComponent } from './module/client/view/panier/panier.component';
-import { CheckoutComponent } from './module/client/view/checkout/checkout.component';
+
 
 
 
@@ -45,26 +27,10 @@ import { CheckoutComponent } from './module/client/view/checkout/checkout.compon
     FooterComponent,
     LoginPageComponent,
     RegisterComponent,
-    // PanierComponent,
-    // CheckoutComponent,
-    // ProductsComponent,
-    // ProductListComponent,
-    // ProductCreateComponent,
-    AuthComponent,
     DemoComponent,
-    // PagesComponent,
-    // UtilsComponent,
-    // ModuleComponent,
-    // ClientComponent,
-    // AdminComponent,
-    // VisitorComponent,
-    // /ViewComponent,
     FormationComponent,
     ProduitBioComponent,
     CongresComponent,
-    // PanierModel,
-    PanierComponent,
-    CheckoutComponent
   ],
   imports: [
     BrowserModule,
@@ -72,7 +38,6 @@ import { CheckoutComponent } from './module/client/view/checkout/checkout.compon
     FormsModule,
     RouterModule,
     ComponentsModule,
-    // ExamplesModule,
     AppRoutingModule,
   ],
   providers: [],
--- paper-kit-2-angular-master/src/app/app.routing.ts
+++ paper-kit-2-angular-master/src/app/app.routing.ts
@@ -2,32 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule, } from '@angular/common';
 import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
-
 import { ComponentsComponent } from './components/components.component';
 import {LoginPageComponent} from './login-page/login-page.component';
 import {RegisterComponent} from './register/register.component';
-import {PanierComponent} from './module/client/view/panier/panier.component';
-// import {CheckoutComponent} from './checkout/checkout.component';
-// import {ProductsComponent} from './module/client/view/products/products.component';
-// import {ProductCreateComponent} from './module/client/view/products/product-create/product-create.component';
-// import {ViewComponent} from './module/admin/view/view.component';
 import {ProduitBioComponent} from './module/client/view/produit-bio/produit-bio.component';
-import {CheckoutComponent} from "./module/client/view/checkout/checkout.component";
 import {FormationComponent} from "./module/client/view/formation/formation.component";
 import {CongresComponent} from "./module/client/view/congres/congres.component";
-// import {AdminComponent} from './module/admin/admin.component';
-// import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
+
 
 const routes: Routes =[
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home',             component: ComponentsComponent },
     { path: 'login',           component: LoginPageComponent },
-    { path: 'panier',          component: PanierComponent },
     { path: 'products',          component: ProduitBioComponent },
-    { path: 'checkout',          component: CheckoutComponent },
     { path: 'formation',          component: FormationComponent },
-    // { path: 'product-create',          component: ProductCreateComponent },
-    // { path: 'module',          component: ViewComponent },
     { path: 'register',          component: RegisterComponent },
     { path: 'congres',          component: CongresComponent },
 
